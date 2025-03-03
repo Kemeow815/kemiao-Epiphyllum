3409792f3ebd5fdcd5306c47815b82f01c3aa808
Masttf <1124677368@qq.com>
2025-03-03 15:26:27 +0800

    添加eslint规则修复build失败

--- src/app/page/[id]/page.tsx
+++ src/app/page/[id]/page.tsx
@@ -6,15 +6,17 @@ export const revalidate = 3600; // ISR 配置（单位：秒）
 
 export async function generateStaticParams() {
     const pages = await getAllPage();
-    
-    return pages.map(item => ({
-      id: item.id.toString()
+
+    return pages.map((item) => ({
+        id: item.id.toString(),
     }));
-  }
-export default async function page({params } : { params : {id: string}}) {
-  const { id } = await params;
-  const pages = await getPageById(parseInt(id));
-    return(
-        <PageCreate pages={pages.content} id={parseInt(id)}></PageCreate>
-    )
+}
+export default async function Page({
+    params,
+}: {
+    params: Promise<{ id: string }>;
+}) {
+    const { id } = await params;
+    const pages = await getPageById(parseInt(id));
+    return <PageCreate pages={pages.content} id={parseInt(id)}></PageCreate>;
 }
