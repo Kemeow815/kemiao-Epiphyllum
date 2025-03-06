fc05ca19162887cb8f1fc7ed11146d96629ca33a
Masttf <1124677368@qq.com>
2025-03-06 14:20:22 +0800

    index

--- src/plugins/rehypeWrapperSpan.js
+++ src/plugins/rehypeWrapperSpan.js
@@ -3,7 +3,7 @@ import { h } from "hastscript";
 
 export function rehypeWrapperSpan() {
     return (tree) => {
-        visit(tree, "element", (node, parent) => {
+        visit(tree, "element", (node, index, parent) => {
             if (parent?.tagName === "pre" && node.tagName === "code") {
                 node.children = [h("span", node.children)];
                 return visit.SKIP;
