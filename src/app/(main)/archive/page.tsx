4551562d93e10d192a161ec173b60b70caa1e065
Masttf <1124677368@qq.com>
2025-03-07 16:03:08 +0800

    Archive全部完成

--- src/app/(main)/archive/page.tsx
+++ src/app/(main)/archive/page.tsx
@@ -5,6 +5,7 @@ export default async function Page() {
     const posts: BlogData[] = await getAllSortedPosts();
     return (
         <div className="card-base px-8 py-6">
+            <div className="mx-auto text-center text-3xl font-bold mb-4">Archive</div>
             <ArchiveCreate posts={posts}></ArchiveCreate>
         </div>
     );
