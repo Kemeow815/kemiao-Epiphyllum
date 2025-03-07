36bbd47cc83a7e72950894ff49bc2b6226e61035
Masttf <1124677368@qq.com>
2025-03-07 16:05:57 +0800

    nothing

--- src/app/(main)/archive/page.tsx
+++ src/app/(main)/archive/page.tsx
@@ -5,7 +5,7 @@ export default async function Page() {
     const posts: BlogData[] = await getAllSortedPosts();
     return (
         <div className="card-base px-8 py-6">
-            <div className="mx-auto text-center text-3xl font-bold mb-4">Archive</div>
+            <div className="mx-auto text-center text-3xl font-bold">Archive</div>
             <ArchiveCreate posts={posts}></ArchiveCreate>
         </div>
     );
