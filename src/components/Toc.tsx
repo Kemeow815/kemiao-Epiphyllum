18eb80550d22feebbdcc7107b2c845c875879575
Masttf <1124677368@qq.com>
2025-03-30 22:05:12 +0800

    修改Toc TocClient

--- src/components/Toc.tsx
+++ src/components/Toc.tsx
@@ -20,7 +20,6 @@ export default async function Toc({
                     <TocContent slug={slug} />
                 </div>
             </div>
-            <TocClient />
         </div>
     );
 }
